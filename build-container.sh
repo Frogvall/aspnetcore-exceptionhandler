24a84b988a6a4f1235c31e0cb8076ddad3a3d585
Frogvall Jonas <jonas.frogvall@scania.com>
2019-04-08 10:41:10 +0200

    Make building possible

--- build-container.sh
+++ build-container.sh
@@ -2,7 +2,7 @@
 set -e
 cd test/aspnetcore-exceptionhandler-test
 dotnet restore
-dotnet xunit -xml ${pwd}/../../testresults/out.xml
+dotnet test --test-adapter-path:. --logger:"xunit;LogFilePath=${pwd}/../../testresults/out.xml"
 cd -
 dotnet pack src/aspnetcore-exceptionhandler/aspnetcore-exceptionhandler.csproj -c release -o ${pwd}/package
 dotnet pack src/aspnetcore-exceptionhandler-awsxray/aspnetcore-exceptionhandler-awsxray.csproj -c release -o ${pwd}/package
