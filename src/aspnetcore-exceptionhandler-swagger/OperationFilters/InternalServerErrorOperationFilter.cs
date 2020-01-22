544c575299d6d2df7de6d2aa410640a9a2776332
Oleksandr Martyniuk <alexander.martinyuk@gmail.com>
2020-01-22 06:09:21 +0100

    Swashbuckle package was updated to the latest version 5.0 (#13)
    Also using OpenAPI

--- src/aspnetcore-exceptionhandler-swagger/OperationFilters/InternalServerErrorOperationFilter.cs
+++ src/aspnetcore-exceptionhandler-swagger/OperationFilters/InternalServerErrorOperationFilter.cs
@@ -1,13 +1,13 @@
-﻿using Swashbuckle.AspNetCore.Swagger;
+﻿using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Frogvall.AspNetCore.ExceptionHandling.OperationFilters
 {
     public class InternalServerErrorOperationFilter : IOperationFilter
     {
-        public void Apply(Operation operation, OperationFilterContext context)
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
         {
-            operation.Responses.Add("500", new Response { Description = "Internal server error" });
+            operation.Responses.Add("500", new OpenApiResponse { Description = "Internal server error" });
         }
     }
 }
